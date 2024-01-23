8dbced1777346e0d288dccc3f9435b05505321c3
Nate <npzaldivar@gmail.com>
2024-01-22 19:56:26 -0700

    Added random movement, timer and score counter

---
+++ main.lua
@@ -0,0 +1,56 @@
+function love.load()
+    -- Target Table
+    target = {}
+    target.x = love.graphics.getWidth() / 2
+    target.y = love.graphics.getHeight() / 2
+    target.radius = 50
+
+    -- Game Values
+    score = 0
+    timer = 10
+
+    -- Game Fonts
+    gameFont = love.graphics.newFont(40)
+end
+
+function love.update(dt)
+    -- Timer
+    if timer > 0 then
+        timer = timer - dt
+    end
+    if timer < 0 then
+        timer = 0
+    end
+end
+
+function love.draw()
+    -- Draw Circle
+    love.graphics.setColor(1,0,0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
+
+    -- Draw Score
+    love.graphics.setFont(gameFont)
+    love.graphics.setColor(1,1,1)
+    love.graphics.print(score, 0, 0)
+
+    -- Draw Timer
+    love.graphics.setFont(gameFont)
+    love.graphics.setColor(1,1,1)
+    love.graphics.print(math.ceil(timer), love.graphics.getWidth() / 2, 0)
+end
+
+-- Code for what happens when the mouse is pressed
+function love.mousepressed( x, y, button, istouch, presses )
+    if button == 1 then
+        local mouseToTarget = distanceBetween(x, y, target.x, target.y)
+        if mouseToTarget < target.radius then
+            score = score + 1
+            target.x = math.random(target.radius, love.graphics.getWidth() - target.radius)
+            target.y = math.random(target.radius, love.graphics.getHeight() - target.radius)
+        end
+    end
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
+end
